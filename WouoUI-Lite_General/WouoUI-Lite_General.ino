fe24b5a00fdf5783c55f4c972b2ad48ddaadebc6
音游玩的人 <115459678+RQNG@users.noreply.github.com>
2023-07-30 14:32:28 +0800

    v 2.3

--- WouoUI-Lite_General/WouoUI-Lite_General.ino
+++ WouoUI-Lite_General/WouoUI-Lite_General.ino
@@ -9,10 +9,6 @@
     * 分辨率 128 * 32 ：在 128 * 64 分辨率的基础上，主界面只保留图标，电压测量页重新设计
     * 通用版本：仅保留列表，主菜单也改为列表，删除电压测量页和与列表无关的动画（保留弹窗效果），经过简单修改可以适配任何分辨率，任何行高度的情况
 
-  WouoUI v2.2 更新内容：
-
-    * 优化动画函数，动画结束后不会再进行无意义的浮点数计算
-
   WouoUI v2 功能：
 
     * 全部使用非线性的平滑缓动动画，包括列表，弹窗，甚至进度条
@@ -244,13 +240,13 @@ M_SELECT setting_menu[]
 M_SELECT about_menu[]
 {
   {"[ WouoUI ]"},
-  {"- Version: v2.2"},
+  {"- Version: v2.3"},
   {"- Board: STM32F103"},
   {"- Ram: 20k"},
   {"- Flash: 64k"},
   {"- Freq: 72Mhz"},
   {"- Creator: RQNG"},
-  {"- Billi UID: 9182439"},  
+  {"- Bili UID: 9182439"},  
 };
 
 /************************************* 页面变量 *************************************/
@@ -300,7 +296,7 @@ struct
 #define   LIST_LINE_H         16                          //列表单行高度
 #define   LIST_TEXT_S         4                           //列表每行文字的上边距，左边距和右边距，下边距由它和字体高度和行高度决定
 #define   LIST_BAR_W          5                           //列表进度条宽度，需要是奇数，因为正中间有1像素宽度的线
-#define   LIST_BOX_R          0.5                         //列表选择框圆角
+#define   LIST_BOX_R          0.5f                        //列表选择框圆角
 */
 
 //超窄行高度测试
@@ -309,7 +305,7 @@ struct
 #define   LIST_LINE_H         7                           //列表单行高度
 #define   LIST_TEXT_S         1                           //列表每行文字的上边距，左边距和右边距，下边距由它和字体高度和行高度决定
 #define   LIST_BAR_W          7                           //列表进度条宽度，需要是奇数，因为正中间有1像素宽度的线
-#define   LIST_BOX_R          0.5                         //列表选择框圆角
+#define   LIST_BOX_R          0.5f                        //列表选择框圆角
 
 struct
 {
@@ -722,8 +718,8 @@ void animation(float *a, float *a_trg, uint8_t n)
 {
   if (*a != *a_trg)
   {
-    if (fabs(*a - *a_trg) < 0.15) *a = *a_trg;
-    else *a += (*a_trg - *a) / (ui.param[n] / 10.0);
+    if (fabs(*a - *a_trg) < 0.15f) *a = *a_trg;
+    else *a += (*a_trg - *a) / (ui.param[n] / 10.0f);
   }
 }
 
